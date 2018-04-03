fa5b6150ae86f5b91247569c2b808de64afd4a3c
g00291461 <g00291461@gmit.ie>
2018-04-03 15:00:17 +0100

    Start Screen Image Rendering

---
+++ UWPgame/UWPgame/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPgame
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        CanvasBitmap StartScreen;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void GameCanvas_CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
+        {
+            //call task asynchronously
+            //won't finish until create resource task is done
+            args.TrackAsyncAction(CreateResourcesAsync(sender).AsAsyncAction());
+        }
+
+        //keeps thread pool alive so process can happen in background
+        async Task CreateResourcesAsync(CanvasControl sender)
+        {
+            //load startscreen image
+            //
+            StartScreen = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/images/startscreen.png"));
+        }
+
+        private void GameCanvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+        {
+            args.DrawingSession.DrawImage(StartScreen);
+
+            //redraw everything on the screen
+            //redraws each frame i.e 60 FPS
+            GameCanvas.Invalidate();
+        }
+    }
+}
