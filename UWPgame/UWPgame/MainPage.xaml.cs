e0f8931fe41d46c5dc578a1c16c0ba4043faa632
g00291461 <g00291461@gmit.ie>
2018-04-04 12:40:21 +0100

    Displaying blasts

--- UWPgame/UWPgame/MainPage.xaml.cs
+++ UWPgame/UWPgame/MainPage.xaml.cs
@@ -8,6 +8,8 @@ using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using UWPgame.Class;
+using Windows.UI;
+using System.Collections.Generic;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -19,7 +21,7 @@ namespace UWPgame
     public sealed partial class MainPage : Page
     {
 
-        public static CanvasBitmap BG, StartScreen, LevelOne, ScoreScreen;
+        public static CanvasBitmap BG, StartScreen, LevelOne, ScoreScreen, Blast;
         public static Rect bounds = ApplicationView.GetForCurrentView().VisibleBounds;
 
         //scaling class can access this info
@@ -34,6 +36,13 @@ namespace UWPgame
         public static int gameState = 0; // Startscreen
 
         public static DispatcherTimer roundTimer = new DispatcherTimer();
+
+        //Lists (Projectile)
+        public static List<float> blastXPOS = new List<float>();
+        public static List<float> blastYPOS = new List<float>();
+
+
+
 
         //constructor
         public MainPage()
@@ -82,6 +91,7 @@ namespace UWPgame
             StartScreen = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/images/startscreen.png"));
             LevelOne = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/images/level-one.png"));
             ScoreScreen = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/images/scorescreen.png"));
+            Blast = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/images/blast.png"));
 
         }
 
@@ -94,6 +104,17 @@ namespace UWPgame
 
             //dynamically changing the image/state
             args.DrawingSession.DrawImage(Scaling.Img(BG));
+            args.DrawingSession.DrawText(countdown.ToString(), 100, 100, Colors.White);
+
+
+            //Display Blasts
+            //Every time we tap on the screen we add 
+            //New blast into the list
+            //Game displays those blasts on the screen
+            for (int i = 0; i < blastXPOS.Count; i++)
+            {
+                args.DrawingSession.DrawImage(Scaling.Img(Blast), blastXPOS[i], blastYPOS[i]);
+            }
 
             //redraw everything on the screen
             //redraws each frame i.e 60 FPS
@@ -106,6 +127,7 @@ namespace UWPgame
             {
                 //show different background
                 gameState = 0; //testing
+                roundEnded = false;
             }
             else
             {
@@ -115,6 +137,11 @@ namespace UWPgame
                 {
                     gameState += 1;
                     roundTimer.Start();
+
+                    //we need to add an item to each of our list
+                    //depending on where we tap on the game canvas
+                    blastXPOS.Add((float)e.GetPosition(GameCanvas).X);
+                    blastYPOS.Add((float)e.GetPosition(GameCanvas).Y);
                 }
 
             }//end else
